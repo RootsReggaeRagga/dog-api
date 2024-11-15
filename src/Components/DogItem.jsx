6a181104df6c57e4f15e439c0cc52897716a5721
bartlomiej.grela <bartlomiej.grela@gmail.com>
2024-11-15 10:53:19 +0100

    update styles, cards and image sizes

--- src/Components/DogItem.jsx
+++ src/Components/DogItem.jsx
@@ -45,11 +45,11 @@ const DogItem = ({ dog }) => {
     return (
 
         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className="flex flex-col items-center pb-10">
-                <img className="w-36 h-36 mt-8 rounded-full shadow-lg object-cover" src={dogOryginalImage} alt="Pies" />
+            <div className="flex flex-col items-center p-5">
+                <img className="w-48 h-48 mb-5 rounded-lg shadow-lg object-cover" src={dogOryginalImage} alt="Pies" />
 
-                <div className="flex mt-4 md:mt-8">
-                    <button onClick={handleButtonClick} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{dog}</button>
+                <div className="flex w-full">
+                    <button onClick={handleButtonClick} className="w-full flex align-center justify-center  px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{dog}</button>
                 </div>
             </div>
 
