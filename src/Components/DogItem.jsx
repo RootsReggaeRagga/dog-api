876e509d1ad309458fb31d3ab65f1d29578e4c84
bartlomiej.grela <bartlomiej.grela@gmail.com>
2024-11-15 10:42:33 +0100

    update images state

--- src/Components/DogItem.jsx
+++ src/Components/DogItem.jsx
@@ -10,7 +10,7 @@ const DogItem = ({ dog }) => {
 
     const fetchDogImage = async () => {
         try {
-            const response = await fetch('https://dog.ceo/api/breeds/image/random');
+            const response = await fetch(dogImageList);
             const data = await response.json();
             setDogImage(data.message);
         } catch (error) {
@@ -29,7 +29,6 @@ const DogItem = ({ dog }) => {
 
 
     const handleButtonClick = () => {
-        fetchDogImage();
         setIsModalOpen(true);
     };
 
@@ -43,18 +42,19 @@ const DogItem = ({ dog }) => {
 
     return (
 
-        <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div class="flex flex-col items-center pb-10">
-                <img class="w-36 h-36 mt-8 rounded-full shadow-lg object-cover" src={dogOryginalImage} alt="Bonnie image" />
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex flex-col items-center pb-10">
+                <img className="w-36 h-36 mt-8 rounded-full shadow-lg object-cover" src={dogOryginalImage} alt="Pies" />
 
-                <div class="flex mt-4 md:mt-8">
-                    <button onClick={handleButtonClick} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{dog}</button>
+                <div className="flex mt-4 md:mt-8">
+                    <button onClick={handleButtonClick} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{dog}</button>
                 </div>
             </div>
 
 
             <DogModal
                 isOpen={isModalOpen}
+                dogOryginalImage={dogOryginalImage}
                 dogImage={dogImage}
                 onClose={closeModal}
                 onFetchNewImage={fetchDogImage}
